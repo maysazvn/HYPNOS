762788941120748c931ba2675f203706ac518b38
maysazvn <maysabmachado84@gmail.com>
2025-10-16 16:50:57 -0300

    menu responsivo pronto

---
+++ javascript/menu.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const menu = document.getElementById("menu");
+  const navLinks = document.getElementById("navLinks");
+
+  menu.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+
+    const icon = menuToggle.querySelector("i");
+    icon.classList.toggle("fa-bars");
+    icon.classList.toggle("fa-xmark");
+  });
+});
