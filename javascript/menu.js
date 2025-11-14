00433f697ffff81a2d1558ed5153aa828f25af79
maysazvn <maysabmachado84@gmail.com>
2025-11-14 16:18:51 -0300

    arrumando coisas

--- javascript/menu.js
+++ javascript/menu.js
@@ -2,11 +2,23 @@ document.addEventListener("DOMContentLoaded", () => {
   const menu = document.getElementById("menu");
   const navLinks = document.getElementById("navLinks");
 
-  menu.addEventListener("click", () => {
+  /* Desabilitar click menu
+  const links = document.querySelectorAll(".navLinks a");
+  for(i=0; i<links.length; i++){
+    links[i].addEventListener("click", function(evt){
+      evt.preventDefault();
+    });
+  }
+  */
+
+  
+
+  menu.addEventListener("click", function(evt){
     navLinks.classList.toggle("active");
 
     const icon = menu.querySelector("i");
     icon.classList.toggle("fa-bars");
     icon.classList.toggle("fa-xmark");
+
   });
 });
