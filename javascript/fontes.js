aa8f8f7246d536c2182a385cbcfa02fe0ff9bea2
maysazvn <maysabmachado84@gmail.com>
2025-11-16 21:09:18 -0300

    yay

--- javascript/fontes.js
+++ javascript/fontes.js
@@ -1,29 +1,40 @@
 function abrirBotao() {
 
   let fontes = document.querySelector(".fontesAzuis");
+  let fontesDois = document.querySelector(".fontesRoxas");
+  let fontesTres = document.querySelector(".fontesBrancas");
+  let fontesQuatro = document.querySelector(".fontesBrancas2");
 
-  if (fontes.style.height == fontes.scrollHeight + "px") {
-    fontes.style.height = "0px";
-
-  } else {
-    fontes.style.height = fontes.scrollHeight + "px";
-
+  if (fontes) {
+    if (fontes.style.height == fontes.scrollHeight + "px") {
+      fontes.style.height = "0px";
+    } else {
+      fontes.style.height = fontes.scrollHeight + "px";
+    }
   }
 
-
-}
-
-function abrirBotao() {
-
-  let fontes = document.querySelector(".fontesRoxas");
-
-  if (fontes.style.height == fontes.scrollHeight + "px") {
-    fontes.style.height = "0px";
-
-  } else {
-    fontes.style.height = fontes.scrollHeight + "px";
-
+  if (fontesDois) {
+    if (fontesDois.style.height == fontesDois.scrollHeight + "px") {
+      fontesDois.style.height = "0px";
+    } else {
+      fontesDois.style.height = fontesDois.scrollHeight + "px";
+    }
   }
 
+  if (fontesTres) {
+    if (fontesTres.style.height == fontesTres.scrollHeight + "px") {
+      fontesTres.style.height = "0px";
+    } else {
+      fontesTres.style.height = fontesTres.scrollHeight + "px";
+    }
+  }
+
+  if (fontesQuatro) {
+    if (fontesQuatro.style.height == fontesQuatro.scrollHeight + "px") {
+      fontesQuatro.style.height = "0px";
+    } else {
+      fontesQuatro.style.height = fontesQuatro.scrollHeight + "px";
+    }
+  }
 
 }
