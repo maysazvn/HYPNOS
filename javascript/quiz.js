9f11ce9cd76e599b410ea4fd3060126597093498
kellermannclara <clarakellermanndossantos@gmail.com>
2025-11-21 18:34:16 -0300

    ajuste no if else

--- javascript/quiz.js
+++ javascript/quiz.js
@@ -5,3 +5,21 @@ if(iniciar == "N" ){
 }else if(iniciar != "S" || iniciar != "N"){
     alert("Valor inválido");
 }
+
+function verificar() {
+
+    let mausHabitos = 0;
+    let insonia = 0;
+    let apneiaSono = 0
+    let narcolepsia = 0;
+    let nenhumDisturbio = 0;
+
+    let p1 = document.querySelector('input[name="p1"]: checked')
+
+    if (p1 && p1.value === "sim") {
+        mausHabitos++
+    } else if (p1 && p1.value === "nao") {
+        nenhumDisturbio++
+    }
+
+}
